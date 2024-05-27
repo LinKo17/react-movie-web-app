51114637104b46f7e78d705da14c15b736f29988
Lin Ko <linkokothein17@gmail.com>
2024-05-27 13:03:47 +0630

    add Error page or 404 page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,24 @@
-function Error(){
+import { Link } from "react-router-dom"
+
+function Error() {
     return (
-        <>
-            <h1>404 not found</h1>
-        </>
+        <div className="d-flex justify-content-center  align-items-center" style={{ height: "100vh" }}>
+
+            <div>
+
+                <h3>
+                    404 | Not Found
+                </h3>
+
+                <div className="text-center mt-3">
+                    <Link to="/">
+                        <button className="btn btn-primary">Home</button>
+                    </Link>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 export default Error
